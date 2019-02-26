773b8c6d3b2d693afbedaf226f320facae3a6d42
dito <dr.dhitz@gmail.com>
2019-02-26 22:02:23 +0700

    Update daftar.php

--- daftar.php
+++ daftar.php
@@ -4,7 +4,7 @@
 </head>
 
 <body>
-    <a href="index.php">Go to Home</a>
+    <a href="index_login.php">Go to Home</a>
     <br/><br/>
 
     <form action="daftar.php" method="post" name="form1" enctype="multipart/form-data">
